e67ef0d4b63746a43c0a665765d0ebd51082bb3c
Adam Stokes <adam.stokes@ubuntu.com>
2015-11-27 21:56:45 -0500

    fix async
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjurelib/async.py
+++ conjurelib/async.py
@@ -25,14 +25,6 @@ work.
 
 import logging
 from concurrent.futures import ThreadPoolExecutor
-import os
-import subprocess
-import errno
-import fcntl
-from tornado.ioloop import IOLoop
-
-BUFSIZ = 1024
-
 log = logging.getLogger("async")
 
 
@@ -40,51 +32,10 @@ AsyncPool = ThreadPoolExecutor(1)
 log.debug('AsyncPool={}'.format(AsyncPool))
 
 
-def _set_nonblocking(fd):
-    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-    fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-
-
-def check_output(call, callback):
-    """ A non-blocking version of subprocess.check_output
-    on the tornado ioloop.
-    """
-    ioloop = IOLoop.instance()
-    out_r, out_w = os.pipe()
-    nul_f = open(os.devnull, 'w')
-    p = subprocess.Popen(call, stdout=out_w, stderr=nul_f)
-    nul_f.close()
-    os.close(out_w)
-    _set_nonblocking(out_r)
-    data = []
-
-    def _poll():
-        if p.returncode is None:
-            p.poll()
-
-        if p.returncode is not None:
-            ioloop.remove_handler(out_r)
-            os.close(out_r)
-
-            callback("".join(data), p.returncode)
-
-    def _handle_read(fd, events):
-        if events & ioloop.READ:
-            try:
-                buf = os.read(out_r, BUFSIZ)
-            except (IOError, OSError) as e:
-                if e.args[0] == errno.EBADF:
-                    _poll()
-                elif e.args[0] not in (errno.EWOULDBLOCK, errno.EAGAIN):
-                    raise
-
-            if not buf:
-                _poll()
-            else:
-                data.append(buf)
-
-        if events & ioloop.ERROR:
-            _poll()
-
-    ioloop.add_handler(out_r, _handle_read,
-                       ioloop.ERROR | ioloop.READ)
+def submit(func, exc_callback):
+    def cb(cb_f):
+        e = cb_f.exception()
+        if e:
+            exc_callback(e)
+    f = AsyncPool.submit(func)
+    f.add_done_callback(cb)
