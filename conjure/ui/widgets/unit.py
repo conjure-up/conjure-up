a221b74bfbe0fbadefe1bc77427b4943e2d1411c
Adam Stokes <adam.stokes@ubuntu.com>
2016-03-10 16:23:20 -0500

    squash! sync bundleplacer, sort controllers, cleanup names

--- conjure/ui/widgets/unit.py
+++ conjure/ui/widgets/unit.py
@@ -1,27 +1,26 @@
-# Copyright 2014-2016 Canonical, Ltd.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-""" Unit info widget
+""" Unit info widget, attaches properties from unit/charm class to
+urwid Text widgets
 """
-
+import logging
 from urwid import WidgetWrap, Text
+
+log = logging.getLogger('unitinfo_w')
 
 
 class UnitInfoWidget(WidgetWrap):
-    def __init__(self, unit):
-        self.agent_name = Text(self.unit.unit_name)
+    def __init__(self, unit, charm_class, hwinfo):
+        self.unit = unit
+        self.charm_class = charm_class
+        self.hwinfo = hwinfo
+        self.container = Text(self.hwinfo['container'])
+        self.machine = Text(self.hwinfo['machine'])
+        self.arch = Text(self.hwinfo['arch'])
+        self.cpu_cores = Text(self.hwinfo['cpu_cores'])
+        self.mem = Text(self.hwinfo['mem'])
+        self.storage = Text(self.hwinfo['storage'])
+        self.display_name = Text(self.charm_class.display_name)
         self.agent_state = Text(self.unit.agent_state)
         self.public_address = Text(self.unit.public_address)
+        self.extended_agent_state = Text(self.unit.extended_agent_state)
+        self.workload_info = Text(self.unit.workload_info)
         self.icon = Text("")
