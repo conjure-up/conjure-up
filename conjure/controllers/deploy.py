4912dca6fc4c16a308eaf22d1a67c2c302fa0605
Adam Stokes <adam.stokes@ubuntu.com>
2016-04-06 15:47:30 -0400

    remove unused code
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/deploy.py
+++ conjure/controllers/deploy.py
@@ -75,26 +75,6 @@ class DeployController:
             mainview.update()
         else:
             pollinate(self.app.session_id, 'PS', self.app.log)
-            # TODO: cleanup a bit
-            # FIXME: needs refinement.
-            if info['ProviderType'] == 'lxd':
-                # Process LXD pre setup scripts
-                topdir = path.join('/usr/share', self.app.config['name'])
-                lxd_profile = path.join(topdir, 'lxd-profile.yaml')
-                pre_sh = path.join(topdir, 'lxd.sh')
-                if path.isfile(pre_sh):
-                    if path.isfile(lxd_profile):
-                        tpl = template.load('lxd-profile.yaml', topdir)
-                        out_tpl = template.save(
-                            tpl, {'lxd_profile': "juju-{}".format(
-                                info['Name'])})
-                        cmd = ("bash {script} \"juju-{name}\" "
-                               "\"{filepath}\"".format(
-                                   name=info['Name'],
-                                   filepath=out_tpl,
-                                   script=pre_sh
-                               ))
-                        shell(cmd)
             self.placement_controller = PlacementController(
                 config=bundleplacer_cfg)
             mainview = PlacerView(self.placement_controller,
