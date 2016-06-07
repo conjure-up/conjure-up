8fba26ae383fcea42b5cffaccf075bb08e830a9a
Michael McCracken <michael.mccracken@gmail.com>
2016-06-07 11:58:34 -0700

    Add juju event queue and finish service walkthru (#84)
    deploys services one at a time during walkthru
    
    adds relations after all are deployed
    
    waits for relations before moving to deploystatus view
    
    Signed-off-by: Michael McCracken <mike.mccracken@canonical.com>

---
+++ conjure/controllers/deploy/common.py
@@ -0,0 +1,14 @@
+from operator import attrgetter
+import os
+
+from bundleplacer.bundle import Bundle
+from conjure.app_config import app
+
+
+def get_bundleinfo():
+    bundle_filename = os.path.join(app.config['spell-dir'], 'bundle.yaml')
+    bundle = Bundle(filename=bundle_filename)
+    services = sorted(bundle.services,
+                      key=attrgetter('service_name'))
+
+    return bundle_filename, bundle, services
--- conjure/controllers/deploy/tui.py
+++ conjure/controllers/deploy/tui.py
@@ -1,15 +1,37 @@
+from concurrent import futures
+import sys
+from functools import partial
+
 from conjure import controllers
 from conjure import utils
 
 from conjure.app_config import app
+from conjure import juju
+
+from .common import get_bundleinfo
+
+this = sys.modules[__name__]
+this.bundle_filename = None
+this.bundle = None
+this.services = []
+
+
+def __handle_exception(tag, exc):
+    utils.error("Error deploying services: {}".format(exc))
+    sys.exit(1)
 
 
 def finish():
     """ handles deployment
 
-    Arguments:
-    back: if true returns to previous controller
     """
+    this.bundle_filename, this.bundle, this.services = get_bundleinfo()
+
+    for service in this.services:
+        juju.deploy_service(service, partial(__handle_exception, "ED"))
+    f = juju.set_relations(this.services,
+                           partial(__handle_exception, "ED"))
+    futures.wait([f])
     utils.pollinate(app.session_id, 'PC')
     controllers.use('deploystatus').render()
 
