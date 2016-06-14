ef36642a2f55165bfcdc61b318574d2eeb6b52bc
Michael McCracken <mike.mccracken@canonical.com>
2016-06-14 23:16:50 +0000

    Improve finding existing controller & adding model
    Finds existing controller even if no current model or controller is
    selected. Doesn't login(), so doesn't require 'juju status' to be
    working to find a controller - it won't be if no model is selected.
    
    Backgrounds creating new model on found controller, so UI is more
    responsive. It's on the juju queue, so deploys will wait for add-model
    to be done.
    
    Signed-off-by: Michael McCracken <mike.mccracken@canonical.com>

--- conjure/controllers/clouds/gui.py
+++ conjure/controllers/clouds/gui.py
@@ -1,10 +1,22 @@
 from conjure.ui.views.cloud import CloudView
+from conjure import async
 from conjure import utils
 from conjure import controllers
 from conjure.app_config import app
 from conjure import juju
 import petname
 from . import common
+
+
+def __handle_exception(exc):
+    utils.pollinate(app.session_id, "E004")
+    app.ui.show_exception_message(exc)
+
+
+def __add_model():
+    juju.switch(app.current_controller)
+    juju.add_model(app.current_model)
+    juju.switch(app.current_model)
 
 
 def finish(cloud):
@@ -14,21 +26,22 @@ def finish(cloud):
     cloud: Cloud to create the controller/model on.
     """
     utils.pollinate(app.session_id, 'CS')
-    have_existing_controller = common.controller_provides_ctype(cloud)
-    if have_existing_controller:
-        app.current_controller = have_existing_controller
-        juju.switch(have_existing_controller)
-        # If a cloud exists create a new model for current deployment
-        app.current_model = petname.Name()
-        juju.add_model(app.current_model)
-        juju.switch(app.current_model)
+    existing_controller = common.get_controller_in_cloud(cloud)
 
-        # Go through the rest of the gui since we already provide a direct
-        # spell path
-        if app.fetcher != 'charmstore-search':
-            return controllers.use('deploy').render()
-        return controllers.use('variants').render()
-    return controllers.use('newcloud').render(cloud)
+    if existing_controller is None:
+        return controllers.use('newcloud').render(cloud)
+
+    app.current_controller = existing_controller
+    app.current_model = petname.Name()
+    async.submit(__add_model,
+                 __handle_exception,
+                 queue_name=juju.JUJU_ASYNC_QUEUE)
+
+    # Go through the rest of the gui since we already provide a direct
+    # spell path
+    if app.fetcher != 'charmstore-search':
+        return controllers.use('deploy').render()
+    return controllers.use('variants').render()
 
 
 def render():
