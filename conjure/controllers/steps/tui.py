8e8b13520eacb8582d7533e92fc8fa288e32fa0b
Adam Stokes <adam.stokes@ubuntu.com>
2016-06-13 21:54:03 -0400

    Rework step findings
    Parse steps that end with yaml and verify that a script is
    found and is executable, log the error if not.
    
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/steps/tui.py
+++ conjure/controllers/steps/tui.py
@@ -1,30 +1,11 @@
-from . import common
-from conjure import utils
 from conjure import controllers
-from conjure.app_config import app
-from glob import glob
-import os
-import sys
-
-this = sys.modules[__name__]
-this.results = []
-
-
-def __fatal(error):
-    utils.error(error)
-    sys.exit(1)
 
 
 def finish():
-    bundle_scripts = os.path.join(
-        app.config['spell-dir'], 'conjure/steps'
-    )
-
-    # post step processing
-    steps = sorted(glob(os.path.join(bundle_scripts, 'step-*')))
-    common.wait_for_steps(steps, utils.info, __fatal)
-
-    return controllers.use('summary').render(this.results)
+    """ We can't really process interactive steps in headless mode. Bypass
+    for now.
+    """
+    return controllers.use('summary').render([])
 
 
 def render():
