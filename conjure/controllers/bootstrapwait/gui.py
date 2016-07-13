16bd6c836c2e4d15eb3673e7fef358a4b80d8227
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-13 14:59:57 -0400

    add additional wait for bootstrap with kitt bar
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/bootstrapwait/gui.py
+++ conjure/controllers/bootstrapwait/gui.py
@@ -1,16 +1,50 @@
 from conjure.ui.views.bootstrapwait import BootstrapWaitView
 from conjure.app_config import app
 from conjure import controllers
+from ubuntui.ev import EventLoop
+from conjure import async
+from conjure import utils
+from functools import partial
+import sys
+
+this = sys.modules[__name__]
 
 
-def finish(deploy_future=None):
-    return controllers.use('deploystatus').render(deploy_future)
+def __handle_exception(tag, exc):
+    utils.pollinate(app.session_id, tag)
+    return app.ui.show_exception_message(exc)
+
+
+def finish(*args):
+    return controllers.use('deploystatus').render()
+
+
+def __wait_for_bootstrap():
+    def check():
+        is_done = False
+        while not is_done:
+            if not app.bootstrap.running:
+                is_done = True
+        return
+
+    f = async.submit(check, partial(__handle_exception, 'ED'))
+    f.add_done_callback(finish)
+
+
+def __refresh(*args):
+    this.view.redraw_kitt()
+    EventLoop.set_alarm_in(1, __refresh)
 
 
 def render(deploy_future=None):
     app.log.debug("Rendering bootstrap wait")
 
-    view = BootstrapWaitView(app)
-    app.ui.set_header(title="Waiting for Bootstrap")
-    app.ui.set_body(view)
-    # finish()
+    this.view = BootstrapWaitView(
+        app=app,
+        message="Juju Controller is initializing. Please wait.")
+    app.ui.set_header(title="Waiting")
+    app.ui.set_body(this.view)
+    if deploy_future:
+        deploy_future.add_done_callback(finish)
+    __refresh()
+    __wait_for_bootstrap()
--- conjure/controllers/bootstrapwait/tui.py
+++ conjure/controllers/bootstrapwait/tui.py
@@ -3,11 +3,11 @@ from conjure.app_config import app
 from conjure import controllers
 
 
-def finish(deploy_future=None):
-    controllers.use('deploystatus').render(deploy_future)
+def finish():
+    controllers.use('deploystatus').render()
 
 
 def render(deploy_future=None):
     while app.bootstrap.running:
-        utils.info(app.bootstrap.output)
-    finish(deploy_future)
+        utils.info("Waiting for bootstrap to finish")
+    finish()
