e0295e4bc4bfa8f67acdc522432dc4968fd618bd
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-20 11:13:31 -0400

    Make service deploy in TUI great again
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/deploy/tui.py
+++ conjure/controllers/deploy/tui.py
@@ -10,65 +10,68 @@ from conjure.app_config import app
 from conjure import juju
 from subprocess import run, PIPE
 
-
-this = sys.modules[__name__]
-this.bundle_filename = None
-this.bundle = None
-this.services = []
+from .common import get_bundleinfo
 
 
-def __handle_exception(tag, exc):
-    utils.error("Error deploying services: {}".format(exc))
-    sys.exit(1)
+class DeployController:
+    def __init__(self):
+        self.bundle_filename = None
+        self.bundle = None
+        self.services = []
 
+    def __handle_exception(self, tag, exc):
+        utils.error("Error deploying services: {}".format(exc))
+        sys.exit(1)
 
-def do_pre_deploy():
-    """ runs pre deploy script if exists
-    """
-    # Set provider type for post-bootstrap
-    app.env['JUJU_PROVIDERTYPE'] = model_info('default')['provider-type']
+    def do_pre_deploy(self):
+        """ runs pre deploy script if exists
+        """
+        # Set provider type for post-bootstrap
+        app.env['JUJU_PROVIDERTYPE'] = model_info('default')['provider-type']
 
-    pre_deploy_sh = os.path.join(app.config['spell-dir'],
-                                 'conjure/steps/00_pre-deploy')
-    if os.path.isfile(pre_deploy_sh) \
-       and os.access(pre_deploy_sh, os.X_OK):
-        utils.pollinate(app.session_id, 'J001')
-        utils.info("Running pre deployment tasks.")
-        try:
-            sh = run(pre_deploy_sh, shell=True,
-                     stdout=PIPE,
-                     stderr=PIPE,
-                     env=app.env)
-            result = json.loads(sh.stdout.decode('utf8'))
-            if result['returnCode'] > 0:
-                utils.error("Failed to run pre-deploy task: "
-                            "{}".format(result['message']))
+        pre_deploy_sh = os.path.join(app.config['spell-dir'],
+                                     'conjure/steps/00_pre-deploy')
+        if os.path.isfile(pre_deploy_sh) \
+           and os.access(pre_deploy_sh, os.X_OK):
+            utils.pollinate(app.session_id, 'J001')
+            utils.info("Running pre deployment tasks.")
+            try:
+                sh = run(pre_deploy_sh, shell=True,
+                         stdout=PIPE,
+                         stderr=PIPE,
+                         env=app.env)
+                result = json.loads(sh.stdout.decode('utf8'))
+                if result['returnCode'] > 0:
+                    utils.error("Failed to run pre-deploy task: "
+                                "{}".format(result['message']))
+                    sys.exit(1)
+
+                utils.info("Finished pre deploy task: {}".format(
+                    result['message']))
+            except Exception as e:
+                utils.error(
+                    "Failed to run pre deploy task: {}".format(e))
                 sys.exit(1)
 
-            utils.info("Finished pre deploy task: {}".format(
-                result['message']))
-        except Exception as e:
-            utils.error(
-                "Failed to run pre deploy task: {}".format(e))
-            sys.exit(1)
+    def finish(self):
+        """ handles deployment
 
+        """
+        for service in self.services:
+            juju.deploy_service(service, utils.info,
+                                partial(self.__handle_exception, "ED"))
 
-def finish():
-    """ handles deployment
+        juju.set_relations(self.services,
+                           utils.info,
+                           partial(self.__handle_exception, "ED"))
 
-    """
-    for service in this.services:
-        juju.deploy_service(service, utils.info,
-                            partial(__handle_exception, "ED"))
+        utils.pollinate(app.session_id, 'PC')
+        controllers.use('deploystatus').render()
 
-    juju.set_relations(this.services,
-                       utils.info,
-                       partial(__handle_exception, "ED"))
+    def render(self):
+        self.bundle_filename, self.bundle, self.services = get_bundleinfo()
 
-    utils.pollinate(app.session_id, 'PC')
-    controllers.use('deploystatus').render()
+        self.do_pre_deploy()
+        self.finish()
 
-
-def render():
-    do_pre_deploy()
-    finish()
+_controller_class = DeployController
