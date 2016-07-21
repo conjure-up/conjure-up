4c44a1eb9d76c1c98667387dae78f1751dbf0b3f
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-21 09:46:22 -0400

    cosmetic
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/summary/tui.py
+++ conjure/controllers/summary/tui.py
@@ -8,7 +8,7 @@ class SummaryController:
 
     def render(self, results):
         common.write_results(results)
-        utils.info("\n\nInstallation of your big software is now complete.")
+        utils.info("Installation of your big software is now complete.")
         self.finish()
 
 _controller_class = SummaryController
