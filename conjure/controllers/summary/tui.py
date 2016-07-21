cc22a83eeaa02f4826552871c0636399fcae6db5
Adam Stokes <adam.stokes@ubuntu.com>
2016-07-21 11:22:36 -0400

    dont call finish, end execution after render of summary
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/controllers/summary/tui.py
+++ conjure/controllers/summary/tui.py
@@ -3,12 +3,8 @@ from conjure import utils
 
 
 class SummaryController:
-    def finish(self):
-        return
-
     def render(self, results):
         common.write_results(results)
         utils.info("Installation of your big software is now complete.")
-        self.finish()
 
 _controller_class = SummaryController
