816adfcd4108d78fb009514c57ab1ceb9a022479
Adam Stokes <battlemidget@users.noreply.github.com>
2016-10-05 14:31:46 -0400

    Fixes #474
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/controllers/summary/tui.py
+++ conjureup/controllers/summary/tui.py
@@ -1,5 +1,6 @@
 import os
-
+from prettytable import PrettyTable
+from termcolor import colored
 from conjureup import utils
 from conjureup.app_config import app
 from conjureup.controllers.summary import common
@@ -13,6 +14,14 @@ class SummaryController:
 
     def render(self, results):
         common.write_results(results, self.save_path)
+        utils.info("Summary")
+        table = PrettyTable()
+        table.field_names = ["Application", "Result"]
+        for k, v in results.items():
+            application_name = colored(k, 'blue', attrs=['bold'])
+            result = colored(v, 'green', attrs=['bold'])
+            table.add_row([application_name, result])
+        print(table)
         utils.info("Installation of your big software is now complete.")
 
 _controller_class = SummaryController
