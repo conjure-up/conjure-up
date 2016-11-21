5ef4a51941ffc3a2dffcce3a6c504e06593caac1
Adam Stokes <battlemidget@users.noreply.github.com>
2016-11-21 09:48:50 -0500

    remove maasclient import in setup
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import setuptools
 
 # All these imported to be added to our distribution
 import conjureup
-import maasclient  # noqa
 import macumba  # noqa
 import ubuntui  # noqa
 import bundleplacer  # noqa
