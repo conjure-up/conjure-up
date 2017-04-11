f1d2b982571e70adc4e97e3c0a9f371da452f6ee
Adam Stokes <battlemidget@users.noreply.github.com>
2017-04-11 08:38:50 -0400

    Fixes #813 - problem syncing spells registry
    Attempts to reset HEAD prior to pulling new spells or switching branches for
    testing. At the very least it will purge the cached spells repo and re-create.
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/utils.py
+++ conjureup/utils.py
@@ -6,6 +6,7 @@ import shutil
 import sys
 import uuid
 from collections import Mapping
+from contextlib import contextmanager
 from pathlib import Path
 from subprocess import (
     DEVNULL,
@@ -26,6 +27,21 @@ from bundleplacer.config import Config
 from conjureup import charm
 from conjureup.app_config import app
 from conjureup.telemetry import track_event
+
+
+@contextmanager
+def chdir(directory):
+    """Change the current working directory to a different directory for a code
+    block and return the previous directory after the block exits. Useful to
+    run commands from a specificed directory.
+
+    :param str directory: The directory path to change to for this context.
+    """
+    cur = os.getcwd()
+    try:
+        yield os.chdir(directory)
+    finally:
+        os.chdir(cur)
 
 
 def run(cmd, **kwargs):
