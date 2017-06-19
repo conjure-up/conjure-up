6cba820e0f6e9356b0d0850c269904d328fa6aeb
Adam Stokes <battlemidget@users.noreply.github.com>
2017-06-18 23:18:58 -0400

    fix tests
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/controllers/lxdsetup/tui.py
+++ conjureup/controllers/lxdsetup/tui.py
@@ -3,9 +3,6 @@ from . import common
 
 class LXDSetupController(common.BaseLXDSetupController):
     def render(self):
-        if self.is_ready:
-            return self.next_screen()
-
         # can't ask, so pick the first (by name) physical interface we find
         self.setup(self.ifaces[0])
 
