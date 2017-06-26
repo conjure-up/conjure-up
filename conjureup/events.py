4bac94c8f0bdd5b74db7c1fda875ac3071c306b0
Adam Stokes <battlemidget@users.noreply.github.com>
2017-06-26 11:14:31 -0400

    Validate ~/.local/share/juju is accessible (#1002)
    * Validate ~/.local/share/juju is accessible
    
    This makes sure that conjure-up can access the juju required files if they
    exist.
    
    Also a drive-by to not send analytics on items in the notrack_exceptions list.
    
    Fixes #991
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>
    
    * Remove PermissionError from notrack_exceptions
    
    Thinking about this more we still want to track permission errors as there is at
    least one more spot where this sort of error shows up.
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/events.py
+++ conjureup/events.py
@@ -143,8 +143,8 @@ def handle_exception(loop, context):
     Error.set()
     exc = context['exception']
 
-    track_exception(str(exc))
     if not (app.noreport or any(pred(exc) for pred in NOTRACK_EXCEPTIONS)):
+        track_exception(str(exc))
         try:
             exc_info = (type(exc), exc, exc.__traceback__)
             app.sentry.captureException(exc_info, tags={
