7238ccb694f811c95447e8f27ae959f66b9d404d
Adam Stokes <battlemidget@users.noreply.github.com>
2017-08-01 14:21:47 -0400

    Also login to provider if loading existing credential
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/controllers/credentials/common.py
+++ conjureup/controllers/credentials/common.py
@@ -1,5 +1,6 @@
 from conjureup import controllers, juju
 from conjureup.app_config import app
+from conjureup.models.provider import VSphere
 
 
 class BaseCredentialsController:
@@ -17,4 +18,17 @@ class BaseCredentialsController:
 
     def finish(self, cred):
         app.current_credential = cred
+
+        # We need to also attempt to login to the provider as we dont
+        # call save_credential.
+        # XXX: is this the best approach?
+        if app.current_cloud_type == 'vsphere' and \
+           not app.vsphere.authenticated:
+            vsphere_creds = juju.get_credential(app.current_cloud, cred)
+            vsphere_cloud = juju.get_cloud(app.current_cloud)
+            vsphere_provider = VSphere()
+            vsphere_provider.endpoint.value = vsphere_cloud['endpoint']
+            vsphere_provider.user.value = vsphere_creds['user']
+            vsphere_provider.password.value = vsphere_creds['password']
+            vsphere_provider.login()
         controllers.use('regions').render()
