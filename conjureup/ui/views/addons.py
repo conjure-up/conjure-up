d2e121d45bffb3beb313a61266d4941682bef90e
Adam Stokes <battlemidget@users.noreply.github.com>
2017-08-18 20:16:36 -0400

    Adds the addon's description in the view
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/ui/views/addons.py
+++ conjureup/ui/views/addons.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 import yaml
-from urwid import CheckBox
+from ubuntui.utils import Padding
+from ubuntui.widgets.hr import HR
+from urwid import CheckBox, Columns, Text
 
 from conjureup.app_config import app
 from conjureup.ui.views.base import BaseView
@@ -27,12 +29,20 @@ class AddonsView(BaseView):
         super().__init__(back)
 
     def build_widget(self):
+        self.choices.append(HR())
         addons_dir = Path(app.config['spell-dir']) / 'addons'
         for addon in sorted(addons_dir.glob('*')):
             metadata = yaml.safe_load((addon / 'metadata.yaml').read_text())
             self.choices.append(CheckBoxValued(metadata.get('friendly-name',
                                                             addon.name),
                                                value=addon.name))
+            self.choices.append(Padding.line_break(""))
+            self.choices.append(
+                Columns([
+                    ('fixed', 3, Text('')),
+                    Text(metadata.get('description', ''))
+                ], dividechars=5)
+            )
         return self.choices
 
     def build_buttons(self):
@@ -42,4 +52,5 @@ class AddonsView(BaseView):
 
     @property
     def selected(self):
-        return [choice.value for choice in self.choices if choice.state]
+        return [choice.value for choice in self.choices
+                if isinstance(choice, CheckBoxValued) and choice.state]
