bd81ea98fee9ef11708a1d1f06f4d7f0d4d18ea9
Adam Stokes <battlemidget@users.noreply.github.com>
2017-08-24 20:54:18 -0400

    Error on missing network, storage pool selection
    It should never get here, but if it does that mean the submit button was pressed
    without a selection from network and storage pool list.
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/controllers/lxdsetup/gui.py
+++ conjureup/controllers/lxdsetup/gui.py
@@ -7,6 +7,10 @@ from conjureup.ui.views.lxdsetup import LXDSetupView
 from . import common
 
 
+class LXDSetupControllerError(Exception):
+    pass
+
+
 class LXDSetupController(common.BaseLXDSetupController):
     async def get_lxd_devices(self):
         devices = {
@@ -18,21 +22,25 @@ class LXDSetupController(common.BaseLXDSetupController):
         self.view.show()
 
     async def set_lxd_info(self, data):
+        if not (data.get('network', None) or data.get('storage-pool', None)):
+            raise LXDSetupControllerError(
+                "Could not determine a network or storage pool "
+                "to continue. Please make sure you have at least "
+                "1 network bridge and 1 storage pool: see `lxc network list` "
+                "and lxc storage list`. (data: {})".format(data))
         net_info = await app.provider.get_network_info(data['network'])
-        app.log.debug(net_info)
-        if net_info:
-            self.set_state('lxd-network-name', net_info['name'])
-            phys_iface_addr = utils.get_physical_network_ipaddr(
-                net_info['name'])
-            iface = ipaddress.IPv4Interface(phys_iface_addr)
-            self.set_state('lxd-network', iface.network)
-            self.set_state('lxd-gateway', iface.ip)
-            self.set_state('lxd-network-dhcp-range-start',
-                           iface.ip + 1)
-            # To account for current interface taking 1 ip
-            number_of_hosts = len(list(iface.network.hosts())) - 1
-            self.set_state('lxd-network-dhcp-range-stop',
-                           "{}".format(iface.ip + number_of_hosts))
+        self.set_state('lxd-network-name', net_info['name'])
+        phys_iface_addr = utils.get_physical_network_ipaddr(
+            net_info['name'])
+        iface = ipaddress.IPv4Interface(phys_iface_addr)
+        self.set_state('lxd-network', iface.network)
+        self.set_state('lxd-gateway', iface.ip)
+        self.set_state('lxd-network-dhcp-range-start',
+                       iface.ip + 1)
+        # To account for current interface taking 1 ip
+        number_of_hosts = len(list(iface.network.hosts())) - 1
+        self.set_state('lxd-network-dhcp-range-stop',
+                       "{}".format(iface.ip + number_of_hosts))
         self.set_state('lxd-storage-pool', data['storage-pool'])
         app.log.debug('LXD Info set: (network: {}) '
                       '(gateway: {}) '
