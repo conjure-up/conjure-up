b0086f017ad9854d0a2c275ebd05fd1a982704fc
Adam Stokes <battlemidget@users.noreply.github.com>
2017-11-30 17:21:58 -0500

    Use proper cache directory in error view
    Fixes #1254
    
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/ui/__init__.py
+++ conjureup/ui/__init__.py
@@ -4,16 +4,19 @@ from conjureup import async
 from conjureup.app_config import app
 from conjureup.ui.views.shutdown import ShutdownView
 from ubuntui.ev import EventLoop
+from pathlib import Path
 
 
 class ConjureUI(Frame):
 
     def show_exception_message(self, ex):
+        _cache_dir = Path(app.argv.cache_dir) / 'conjure-up.log'
         errmsg = str(ex)
-        errmsg += ("\n\n"
-                   "Review log messages at ~/.cache/conjure-up/conjure-up.log "
-                   "If appropriate, please submit a bug here: "
-                   "https://github.com/conjure-up/conjure-up/issues/new")
+        errmsg += (
+            "\n\n Review log messages at {} "
+            "If appropriate, please submit a bug here: "
+            "https://github.com/conjure-up/conjure-up/issues/new".format(
+                _cache_dir))
 
         async.shutdown()
         EventLoop.remove_alarms()
