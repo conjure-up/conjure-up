2b2de68f89ef6e710ae36c615697c326cd26e772
Cory Johns <johnsca@gmail.com>
2018-01-16 07:42:13 -0500

    Prompt to confirm before quit
    Fixes #1291

--- conjureup/ui/__init__.py
+++ conjureup/ui/__init__.py
@@ -1,10 +1,13 @@
+from pathlib import Path
+
+from ubuntui.ev import EventLoop
 from ubuntui.frame import Frame  # noqa
 from ubuntui.views import ErrorView
+from urwid import Overlay
+
 from conjureup import async
 from conjureup.app_config import app
 from conjureup.ui.views.shutdown import ShutdownView
-from ubuntui.ev import EventLoop
-from pathlib import Path
 
 
 class ConjureUI(Frame):
@@ -28,5 +31,14 @@ class ConjureUI(Frame):
     def show_error_message(self, msg):
         self.frame.body = ErrorView(msg)
 
-    def show_shutdown_message(self):
-        self.frame.body = ShutdownView()
+    def show_shutdown_dialog(self, exit_code):
+        self.frame.body = Overlay(ShutdownView(exit_code),
+                                  self.frame.body,
+                                  'center', ('relative', 45),
+                                  'middle', 'pack')
+
+    def hide_shutdown_dialog(self):
+        self.frame.body = self.frame.body.bottom_w
+
+    def quit(self, exit_code=0):
+        self.show_shutdown_dialog(exit_code)
