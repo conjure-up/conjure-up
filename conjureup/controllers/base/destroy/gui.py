598827926b5d908405d39004afc1eb8929007a50
Adam Stokes <battlemidget@users.noreply.github.com>
2018-07-24 19:29:36 -0400

    pull in spells from model config
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- conjureup/controllers/base/destroy/gui.py
+++ conjureup/controllers/base/destroy/gui.py
@@ -1,27 +1,70 @@
-from conjureup import errors, controllers
-from conjureup.models.provider import load_schema
+import os
+import json
+from conjureup import controllers, juju, utils, snap
 from conjureup.app_config import app
 from conjureup.ui.views.destroy import DestroyView
+from conjureup.models.step import StepModel
+from juju.model import Model
 
 
 class Destroy:
 
     def __init__(self):
         self.view = None
+        self.spells = []
 
-    def finish(self, selection):
-        if selection['type'] == 'juju':
-            app.provider = load_schema(selection['cloud'])
+    async def _query_model_config(self, controller, model):
+        if not app.juju.client:
+            app.juju.client = Model(app.loop)
+        await app.juju.client.connect("{}:{}".format(
+            controller, model))
+        result = await app.juju.client.get_config()
+        if 'extra-info' in result:
             try:
-                app.provider.load(selection['cloud'])
-            except errors.SchemaCloudError as e:
-                raise e
-        return controllers.use('destroyconfirm').render(selection)
+                return json.loads(result['extra-info'].value)
+            except json.JSONDecodeError as e:
+                app.log.debug("Failed to read a model config: {}".format(e))
+        return None
 
-    def render(self):
+    def finish(self, spellname):
+        for spell in self.spells:
+            if spellname == spell['spellname']:
+                if 'controller' in spell:
+                    app.provider.controller = spell['controller']
+                if 'model' in spell:
+                    app.provider.model = spell['model']
+
+        utils.set_chosen_spell(spellname,
+                               os.path.join(app.conjurefile['cache-dir'],
+                                            spellname))
+        utils.set_spell_metadata()
+        StepModel.load_spell_steps()
+        controllers.setup_metadata_controller()
+        return controllers.use('destroyconfirm').render()
+
+    async def query_deployments(self):
+        """ Get a list of deployed spells
+        """
+        existing_controllers = juju.get_controllers()['controllers']
+        for cname in existing_controllers.keys():
+            for model in juju.get_models(cname)['models']:
+                model_config = await self._query_model_config(
+                    cname,
+                    model['short-name'])
+                self.spells.append(
+                    {'spellname': model_config['config']['spell'],
+                     'controller': cname,
+                     'model': model})
+        if snap.is_installed('microk8s'):
+            self.spells.append({'spellname': 'microk8s'})
+
         self.view = DestroyView(app,
+                                spells=self.spells,
                                 cb=self.finish)
         self.view.show()
+
+    def render(self):
+        app.loop.create_task(self.query_deployments())
 
 
 _controller_class = Destroy
--- conjureup/ui/views/destroy.py
+++ conjureup/ui/views/destroy.py
@@ -1,4 +1,3 @@
-from conjureup import juju
 from conjureup.ui.views.base import BaseView
 from conjureup.ui.widgets.selectors import MenuSelectButtonList
 
@@ -8,51 +7,18 @@ class DestroyView(BaseView):
     subtitle = "Please choose a deployed spell to destroy"
     show_back_button = False
 
-    def __init__(self, app, show_snaps, cb):
+    def __init__(self, app, spells, cb):
         self.app = app
         self.cb = cb
-        self.deployed_map = {}
-        self.show_snaps = show_snaps
+        self.spells = spells
         self.config = self.app.config
-        existing_controllers = juju.get_controllers()['controllers']
-        for cname, d in existing_controllers.items():
-            self.deployed_map[cname] = {'controller': d}
-            self.deployed_map[cname].update(**juju.get_models(cname))
         super().__init__()
 
     def build_widget(self):
         # NB: I dont like this currently, but leaving in for now.
         widget = MenuSelectButtonList()
-        if self.deployed_map:
-            for name, deploy in self.deployed_map.items():
-                value_map = {
-                    'type': 'juju',
-                    'cloud': deploy['controller']['cloud'],
-                    'controller': name
-                }
-                widget.append_option(
-                    label="{} ({})".format(
-                        name,
-                        deploy['controller']['cloud']),
-                    value=value_map)
-                for model in deploy['models']:
-                    if model['name'] == 'admin/controller':
-                        continue
-                    if model['life'] != 'alive' or \
-                       model['status']['current'] != 'available':
-                        continue
-                    value_map['model'] = model['short-name']
-                    widget.append_option(
-                        label="- {}".format(
-                            model['short-name']),
-                        value=value_map)
-        if self.show_snaps:
-            value_map = {
-                'type': 'snap',
-                'snap': 'microk8s'
-            }
-            widget.append_option(label='microk8s (localhost)',
-                                 value=value_map)
+        for spell in self.spells:
+            widget.append_option(spell['spellname'])
         widget.select_first()
         return widget
 
