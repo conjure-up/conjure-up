dfc42d3a06d0d8fdc08cc3f19670122fc760c581
Cory Johns <johnsca@gmail.com>
2018-07-25 15:16:41 -0400

    Handle missing extra-info and connection errors

--- conjureup/controllers/base/destroy/gui.py
+++ conjureup/controllers/base/destroy/gui.py
@@ -1,9 +1,11 @@
+import asyncio
 import os
 import json
 from conjureup import controllers, juju, utils, snap
 from conjureup.app_config import app
 from conjureup.ui.views.destroy import DestroyView
 from conjureup.models.step import StepModel
+from juju.errors import JujuConnectionError
 from juju.model import Model
 
 
@@ -16,15 +18,31 @@ class Destroy:
     async def _query_model_config(self, controller, model):
         if not app.juju.client:
             app.juju.client = Model(app.loop)
-        await app.juju.client.connect("{}:{}".format(
-            controller, model))
+        target = "{}:{}".format(controller, model)
+        try:
+            # TODO: this could take a while; we should show an interstitial
+            await asyncio.wait_for(app.juju.client.connect(target), 30)
+        except (JujuConnectionError, asyncio.TimeoutError):
+            app.log.debug('Unable to connect to {}; skipping'.format(target))
+            return {}
         result = await app.juju.client.get_config()
         if 'extra-info' in result:
+            extra_info = result['extra-info'].value
+        else:
+            extra_info = None
+        app.log.debug('extra-info for {}:{}: {}'.format(controller,
+                                                        model,
+                                                        extra_info))
+        if extra_info:
             try:
-                return json.loads(result['extra-info'].value)
-            except json.JSONDecodeError as e:
-                app.log.debug("Failed to read a model config: {}".format(e))
-        return None
+                result = json.loads(result['extra-info'].value)
+                if isinstance(result, dict):
+                    return result
+            except json.JSONDecodeError:
+                pass
+        app.log.debug("Ignoring unintelligble model config for {}:{}: "
+                      "{}".format(controller, model, extra_info))
+        return {}
 
     def finish(self, spellname):
         for spell in self.spells:
@@ -47,12 +65,18 @@ class Destroy:
         """
         existing_controllers = juju.get_controllers()['controllers']
         for cname in existing_controllers.keys():
-            for model in juju.get_models(cname)['models']:
+            # TODO: this could take a while; we should show an interstitial
+            models = juju.get_models(cname)
+            if not models:
+                continue
+            for model in models['models']:
                 model_config = await self._query_model_config(
                     cname,
                     model['short-name'])
+                spell_name = model_config.get('config', {}).get('spell',
+                                                                '(unknown)')
                 self.spells.append(
-                    {'spellname': model_config['config']['spell'],
+                    {'spellname': spell_name,
                      'controller': cname,
                      'model': model})
         if snap.is_installed('microk8s'):
