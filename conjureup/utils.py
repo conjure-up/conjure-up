e85b3c772a64a6950a998828c1b4331d1a8d921f
Cory Johns <johnsca@gmail.com>
2019-01-24 10:42:48 -0500

    Fix lint errors

--- conjureup/utils.py
+++ conjureup/utils.py
@@ -612,7 +612,7 @@ def is_valid_hostname(hostname):
     if hostname[-1] == ".":
         # strip exactly one dot from the right, if present
         hostname = hostname[:-1]
-    allowed = re.compile("(?!-)[A-Z\d\-\_]{1,63}(?<!-)$", re.IGNORECASE)
+    allowed = re.compile(r"(?!-)[A-Z\d\-\_]{1,63}(?<!-)$", re.IGNORECASE)
     return all(allowed.match(x) for x in hostname.split("."))
 
 
